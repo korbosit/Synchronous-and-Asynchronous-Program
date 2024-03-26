7f2b43882ba1d7947c0dd242f8f089307888d664
Vlaimir Korba <krissky1986@gmail.com>
2024-03-26 11:10:20 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+//===Synchronous and Asynchronous Program=====================================================================================================================================================
+
+// Asynchronous Javascript and AJAX
+
+// Sunchronous program
+// let HelloWorld = function () {
+//     console.log("Hello World!");
+// };
+
+// let greetings = function () {
+//     console.log("Good Morning");
+//     HelloWorld();
+//     console.log("Have a good day!");
+// };
+
+// greetings();
+
+// let str = "Good Morning";
+// console.log(str);
+// alert("This is an alert window!");
+// console.log("Have a good day");
+
+//========================================================================================================================================================
+
+// Asynchronous Program
+
+let str = "Good Morning";
+console.log(str);
+setTimeout(() => {
+    alert("This is an alert window!");
+}, 5000);
+console.log("Have a good day");
